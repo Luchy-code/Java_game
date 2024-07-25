0055f3bca03cba99c8f6e55e1c7b029aa2c25a88
Luchy <87878147+Luchy-code@users.noreply.github.com>
2024-07-25 02:28:41 -0300

    initial commit

---
+++ src/juego/Obstaculo.java
@@ -0,0 +1,29 @@
+package juego;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+
+//import java.awt.Color;
+import java.awt.Image;
+
+public class Obstaculo {
+
+	double x;
+	double y;
+	double radio;
+	Image imagen;
+	double angulo;
+
+	public Obstaculo(double x, double y, double radio) {
+		this.x = x;
+		this.y = y;
+		this.radio = radio;
+		this.imagen = Herramientas.cargarImagen("arbusto.png");
+	}
+
+	public void dibujarse(Entorno entorno) {
+		entorno.dibujarImagen(imagen, x, y, angulo);
+		// entorno.dibujarCirculo(x, y, radio, Color.GRAY);
+	}
+
+}
